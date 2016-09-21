4dac1caa2a011d2424f144f9e9ea667ddd98c5c2
czarq92 <czarq92@o2.pl>
2016-09-21 23:48:13 +0200

    Zabawa z obsługą przycisków i akcji. Zablokowanie zmiany położenia aplikacji.

--- app/src/main/java/firstapp/com/firstapp/MainActivity.java
+++ app/src/main/java/firstapp/com/firstapp/MainActivity.java
@@ -1,31 +1,62 @@
 package firstapp.com.firstapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.util.Random;
+import java.util.concurrent.locks.ReadWriteLock;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
+    FloatingActionButton fab;
+
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setBackgroundColor(Color.RED);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Czego tu szukasz, Pajacu ?!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("DEBUG", "Onclick in button startButton");
+
+                i++;
+
+                fab.setRotation(fab.getRotation() + 30);
+
+                Toast.makeText(getApplicationContext(), "BANG! [" + i + "]", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
